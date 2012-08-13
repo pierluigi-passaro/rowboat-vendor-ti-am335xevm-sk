11fc384fd7fb57fe43559ce3f6e46a4d9206cd75
Shakti Tiwari <shakti.tiwari@ti.com>
2012-08-13 11:32:56 +0530

    Add media_codecs.xml file
     This is required for playing videos
    
    Change-Id: I93d2c74652bc8ef9a16c3ca431af559e1c99468d
    Signed-off-by: Shakti Tiwari <shakti.tiwari@ti.com>

--- device.mk
+++ device.mk
@@ -19,7 +19,8 @@ PRODUCT_COPY_FILES := \
 	device/ti/am335xevm_sk/init.am335xevm_sk.rc:root/init.am335xevm.rc \
 	device/ti/am335xevm_sk/init.am335xevm_sk.usb.rc:root/init.am335xevm_sk.usb.rc \
 	device/ti/am335xevm_sk/ueventd.am335xevm_sk.rc:root/ueventd.am335xevm_sk.rc \
-	device/ti/am335xevm_sk/vold.fstab:system/etc/vold.fstab
+	device/ti/am335xevm_sk/vold.fstab:system/etc/vold.fstab \
+	device/ti/am335evm_sk/media_codecs.xml:system/etc/media_codecs.xml
 
 # KeyPads
 PRODUCT_COPY_FILES += \
