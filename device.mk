8b61db6ae27b17938ca40c330756e83d3d839085
Abhijit Kumbhar <x0112741@ti.com>
2012-11-01 15:27:56 -0500

    Enable WiFi Direct support in JB
    Signed-off-by: Abhijit Kumbhar <x0112741@ti.com>

--- device.mk
+++ device.mk
@@ -38,7 +38,8 @@ PRODUCT_COPY_FILES += \
 
 # WLAN support
 PRODUCT_COPY_FILES +=\
-	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml
+	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+	frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml
 
 # Accelerometer support
 PRODUCT_COPY_FILES += \
